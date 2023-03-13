b5d5c94ac12f966846531b2fee2ee9cf33ae5edd
Hashim <hashimabdulmujib@gmail.com>
2023-03-13 07:03:18 +0100

    irst commit

---
+++ backend/src/lambda/http/deleteTodo.ts
@@ -0,0 +1,31 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { deleteTodo } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    // Remove a TODO item by id
+    const userId = getUserId(event)
+    const resp = await deleteTodo({ todoId, userId })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: resp
+      })
+    }
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
---
+++ backend/src/requests/DeleteTodoRequest.ts
@@ -0,0 +1,7 @@
+/**
+ * Fields in a request to deprecate a single TODO item.
+ */
+export interface DeleteTodoRequest {
+    todoId: string
+    userId: string
+  }
